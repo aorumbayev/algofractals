d4fb9372244f8930834575a8f31073720ea17adc
MillionAlgosFather <info@algoworld.io>
2022-01-17 16:07:24 +0100

    Adding sentry

--- src/main.js
+++ src/main.js
@@ -6,12 +6,30 @@ import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
 import Particles from "particles.vue3";
 import mitt from "mitt";
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+
 const emitter = mitt();
 const app = createApp(App);
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+Sentry.init({
+    app,
+    dsn: "https://c94881e975e34aefa866f836db5f0834@o855233.ingest.sentry.io/6153173",
+    integrations: [
+        new Integrations.BrowserTracing({
+            routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+            tracingOrigins: ["localhost", "algofractals.com", /^\//],
+        }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 });
 
 app.use(router);
